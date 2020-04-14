732e04e962e977f5b564c8793e8a614893abd05b
IvanMartir <63245838+IvanMartir@users.noreply.github.com>
2020-04-14 19:42:54 +0200

    Fet un tutorial de moviment al inici del joc i pujat video

--- Assets/Scripts/Bandit.cs
+++ Assets/Scripts/Bandit.cs
@@ -22,8 +22,11 @@ public class Bandit : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        //Check if character just landed on the ground
-        if (!m_grounded && m_groundSensor.State()) {
+
+        if (DialogManager.ShowingDialogs == false)
+        {
+            //Check if character just landed on the ground
+            if (!m_grounded && m_groundSensor.State()) {
             m_grounded = true;
             m_animator.SetBool("Grounded", m_grounded);
         }
@@ -34,19 +37,20 @@ public class Bandit : MonoBehaviour {
             m_animator.SetBool("Grounded", m_grounded);
         }
 
-        if (DialogManager.ShowingDialogs == false)
-        {
             // -- Handle input and movement --
             float inputX = Input.GetAxis("Horizontal");
 
             // Swap direction of sprite depending on walk direction
             if (inputX > 0)
+            {
                 GetComponent<SpriteRenderer>().flipX = false;
+                m_body2d.velocity = new Vector2(inputX * m_speed, m_body2d.velocity.y);
+            }
             else if (inputX < 0)
+            {
                 GetComponent<SpriteRenderer>().flipX = true;
-
-            // Move
-            m_body2d.velocity = new Vector2(inputX * m_speed, m_body2d.velocity.y);
+                m_body2d.velocity = new Vector2(inputX * m_speed, m_body2d.velocity.y);
+            }
 
 
             //Set AirSpeed in animator
@@ -88,6 +92,8 @@ public class Bandit : MonoBehaviour {
                 m_groundSensor.Disable(0.2f);
             }
 
+            
+
             //Run
             else if (Mathf.Abs(inputX) > Mathf.Epsilon)
                 m_animator.SetInteger("AnimState", 2);
@@ -95,6 +101,11 @@ public class Bandit : MonoBehaviour {
             //Combat Idle
             else if (m_combatIdle)
                 m_animator.SetInteger("AnimState", 1);
+
+            else if(Mathf.Abs(inputX) < Mathf.Epsilon && !m_combatIdle)
+            {
+                m_animator.SetInteger("AnimState", 0);
+            }
         }
 
         //Idle
---
+++ Assets/Scripts/TutorialKeys.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialKeys : MonoBehaviour
+{
+
+    bool EnabledObjects = false;
+
+    public GameObject A_Key;
+    public GameObject D_Key;
+    public GameObject SPACE_Key;
+    public GameObject ScaleBar;
+    public GameObject Wall;
+
+    public Image ScaleImage;
+
+    bool doingA = false;
+    bool doingD = false;
+    bool doingSPACE = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnabledObjects = false;
+        D_Key.SetActive(false);
+        A_Key.SetActive(false);
+        SPACE_Key.SetActive(false);
+        ScaleBar.SetActive(false);
+        Wall.SetActive(false);
+        ScaleImage.transform.localScale = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        // Check if Dialogs already ended
+        if(DialogManager.ShowingDialogs == false)
+        {
+
+            if(EnabledObjects == false)
+            {
+                EnabledObjects = true;
+                ScaleBar.SetActive(true);
+                Wall.SetActive(true);
+                ScaleImage.transform.localScale = new Vector3(0.02f, 1, 1);
+                doingD = true;
+            }
+
+        }
+
+        if (doingD == true)
+        {
+            D_Key.SetActive(true);
+
+            if (ScaleImage.transform.localScale.x > 0.90f)
+            {
+                doingD = false;
+                doingA = true;
+                A_Key.SetActive(true);
+                D_Key.SetActive(false);
+                ScaleImage.transform.localScale = new Vector3(0.02f, 1, 1);
+            }
+       
+            if(Input.GetKey(KeyCode.D))
+            {
+                ScaleImage.transform.localScale = new Vector3(ScaleImage.transform.localScale.x + 0.01f, 1, 1);
+            }
+        }
+
+        if (doingA == true)
+        {
+            A_Key.SetActive(true);
+
+            if (ScaleImage.transform.localScale.x > 0.90f)
+            {
+                doingA = false;
+                doingSPACE = true;
+                A_Key.SetActive(false);
+                SPACE_Key.SetActive(true);
+                ScaleImage.transform.localScale = new Vector3(0.02f, 1, 1);
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                ScaleImage.transform.localScale = new Vector3(ScaleImage.transform.localScale.x + 0.01f, 1, 1);
+            }
+        }
+
+        if(doingSPACE == true)
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                doingSPACE = false;
+                SPACE_Key.SetActive(false);
+                ScaleImage.transform.localScale = new Vector3(0, 0, 0);
+                ScaleBar.SetActive(false);
+                Wall.SetActive(false);
+            }
+        }
+
+    }
+}
